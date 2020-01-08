280d09d5461cf16c264079f334217a84a895fd7a
adamkacz <59448065+adamkacz@users.noreply.github.com>
2020-01-08 16:31:53 +0100

    Add files via upload

--- Usluga.cs
+++ Usluga.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Projekt
 {
-    class Usluga
+    abstract class Usluga
     {
+        double koszt;
+        string nazwa;
+
+        public double Koszt { get => koszt; set => koszt = value; }
+        public string Nazwa { get => nazwa; set => nazwa = value; }
+
+        public Usluga()
+        {
+            Koszt = 0;
+            Nazwa = "";
+        }
+
+        public Usluga(string nazwa, double koszt)
+        {
+            Nazwa = nazwa;
+            Koszt = koszt;
+        }
+        
     }
 }
