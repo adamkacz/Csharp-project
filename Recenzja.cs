2f6bc682439be61ffe15e12cf661a683a78e81f5
mwarczynska <44765742+mwarczynska@users.noreply.github.com>
2020-01-08 18:08:06 +0100

    Add files via upload

---
+++ Recenzja.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Recenzja
+    {
+        string opis;
+        KontoUzytkownika uzytkownik;
+
+        public string Opis { get => opis; set => opis = value; }
+        internal KontoUzytkownika Uzytkownik { get => uzytkownik; set => uzytkownik = value; }
+
+        public Recenzja()
+        {
+            Opis = "";
+            Uzytkownik = new KontoUzytkownika();
+
+        }
+
+        public Recenzja(string opis, KontoUzytkownika uzytkownik)
+        {
+            Opis = opis;
+            Uzytkownik = uzytkownik;
+        }
+
+        public override string ToString()
+        {
+            return $" Użytkownik: {uzytkownik} RECENZJA: {opis}";
+        }
+    }
+}
